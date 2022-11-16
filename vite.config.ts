30a1efe62ad0ba16020915a957ae3172fe656174
xavifortes <itsfortes@gmail.com>
2022-11-16 09:00:43 +0100

    modified vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,35 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import viteCompression from "vite-plugin-compression";
+
+export default () => {
+  return defineConfig({
+    plugins: [vue(), vueJsx(), viteCompression()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:5000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    build: {
+      outDir: "dist",
+      assetsDir: "assets",
+      chunkSizeWarningLimit: 1000,
+    },
+  });
+};
 
 // https://vitejs.dev/config/
+/*
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
@@ -13,3 +40,4 @@ export default defineConfig({
     },
   },
 });
+*/
