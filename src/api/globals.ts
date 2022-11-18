7a976dd179f70a0cb319d4e5b7f7544bad4996ce
xavifortes <itsfortes@gmail.com>
2022-11-18 09:44:05 +0100

    changed url

--- src/api/globals.ts
+++ src/api/globals.ts
@@ -1,3 +1,3 @@
 // Create a global variable to hold the base url
-//export const baseUrl = "https://api.karasu.es";
-export const baseUrl = "http://localhost:8080";
+export const baseUrl = "https://api.karasu.es";
+//export const baseUrl = "http://localhost:8080";
